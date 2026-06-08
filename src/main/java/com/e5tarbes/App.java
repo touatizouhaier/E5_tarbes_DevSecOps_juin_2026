a38899a5d61a68cf0db8fa8f71ec510547f29ba9
touatizouhaier <83752620+touatizouhaier@users.noreply.github.com>
2026-06-08 15:46:37 +0200

    Create App.java

---
+++ src/main/java/com/e5tarbes/App.java
@@ -0,0 +1,44 @@
+package com.e5tarbes;
+
+
+/*
+ Application Java simple
+ utilisée pour tester Jenkins CI/CD
+*/
+
+
+public class App {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println(
+            "================================="
+        );
+
+
+        System.out.println(
+            "Application E5 Tarbes DevSecOps"
+        );
+
+
+        System.out.println(
+            "Pipeline Jenkins GitHub OK"
+        );
+
+
+        System.out.println(
+            "Build Maven réussi"
+        );
+
+
+        System.out.println(
+            "================================="
+        );
+
+
+    }
+
+
+}
